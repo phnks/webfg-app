3cac2badcc5c371985c0983e0ed2ffa066fa4655
anum <anum@example.com>
2025-04-23 23:46:31 +0000

    scripts dont use tty

--- webfg-gm-app/scripts/deploy.sh
+++ webfg-gm-app/scripts/deploy.sh
@@ -54,10 +54,12 @@ npm run "$BUILD_SCRIPT"
 # --- Deploy Stack ---
 echo "Deploying stack: ${STACK_NAME}..."
 sam deploy \
+  --no-confirm-changeset \
+  --no-progressbar \
+  --no-fail-on-empty-changeset \
   --stack-name "${STACK_NAME}" \
   --parameter-overrides Environment="${ENVIRONMENT}" DeploymentId="${DEPLOYMENT_ID}" \
   --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
-  --no-fail-on-empty-changeset
 
 # --- Sync S3 ---
 echo "Retrieving bucket name..."
